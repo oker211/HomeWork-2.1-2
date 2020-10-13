007a836484205e1b4d86f27c58296b902b4700bd
Filipp Slabodetsky <oker211@gmail.com>
2020-10-13 13:46:18 +0300

    HW 2.1 (2)

--- HomeWork 2.1 (2)/ViewController.swift
+++ HomeWork 2.1 (2)/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Закругляем углы у вью и кнопки
+        redView.layer.cornerRadius = 50
+        yellowView.layer.cornerRadius = 50
+        greenView.layer.cornerRadius = 50
+        nextButton.layer.cornerRadius = 10
+        
+        // Устанавливаем цвет Альфа у вью
+        redView.alpha = 0.5
+        yellowView.alpha = 0.5
+        greenView.alpha = 0.5
+        
     }
 
-
+    @IBAction func pressButton() {
+        if greenView.alpha == 0.5 && yellowView.alpha == 0.5 && redView.alpha == 0.5 {
+            redView.alpha = 1.0
+        } else if greenView.alpha == 0.5 && yellowView.alpha == 0.5 && redView.alpha == 1.0 {
+            redView.alpha = 0.5
+            yellowView.alpha = 1.0
+        } else if greenView.alpha == 0.5 && yellowView.alpha == 1.0 && redView.alpha == 0.5 {
+            yellowView.alpha = 0.5
+            greenView.alpha = 1.0
+        } else if greenView.alpha == 1 && yellowView.alpha == 0.5 && redView.alpha == 0.5 {
+            greenView.alpha = 0.5
+            redView.alpha = 1.0
+        }
+    
 }
 
+}
